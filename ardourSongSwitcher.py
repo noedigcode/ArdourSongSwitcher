11713e7d9a4f3dfbb3b3546082a5a672065ae58c
Gideon van der Kolf <vanderkolfg@gmail.com>
2016-08-09 08:34:19 +0200

    - CC messages only respond to data2=127 now. - Some useful info is printed on startup - User can now easily choose ALSA/Jack backend and a port to automatically   connect the MIDI input port to.

--- ardourSongSwitcher.py
+++ ardourSongSwitcher.py
@@ -27,6 +27,12 @@ cc_playPause = 36
 cc_stop = 37
 cc_toCurrentSongStart = 38
 
+# Mididings MIDI backend: 'alsa' or 'jack'
+backend='alsa'
+
+# Client to autoconnect the input MIDI port to
+autoconnect='VMPK Output:0'
+
 #==============================================================================
 # End of user edit section
 #==============================================================================
@@ -101,49 +107,53 @@ ardour_switchSong(currentSong)
 def mididingsProcess(n):
     global currentSong
     if (n.type == CTRL):
-        if n.data1 == cc_next:
-            # Next song
-            print('CC received: Next song')
-            if currentSong < len(songs)-1:
-                currentSong += 1
-            # Switch to next song in Ardour
-            print('   Switching to song %d in Ardour'%currentSong)
-            ardour_switchSong(currentSong)
-            # Return program event to switch Mididings scene
-            print('   Switching to scene %d'%(currentSong+1))
-            return  event.ProgramEvent(1,1,currentSong+1)
-        
-        elif n.data1 == cc_prev:
-            # Previoius song
-            print('CC received: Previous song')
-            if currentSong > 0:
-                currentSong -= 1
-            # Switch to previous song in Ardour
-            print('   Switching to song %d in Ardour'%currentSong)
-            ardour_switchSong(currentSong)
-            # Return program event to switch Mididings scene
-            print('   Switching to scene %d'%(currentSong+1))
-            return event.ProgramEvent(1,1,currentSong+1)
-        
-        elif n.data1 == cc_play:
-            # Play
-            print('CC received: Play')
-            ardour_play()
-        
-        elif n.data1 == cc_playPause:
-            # Play/Pause
-            print('CC received: Play/Pause')
-            ardour_playPause()
-        
-        elif n.data1 == cc_stop:
-            # Stop
-            print('CC received: Stop')
-            ardour_stop()
+        if (n.data2 == 127):
+            if n.data1 == cc_next:
+                # Next song
+                print('CC received: Next song')
+                if currentSong < len(songs)-1:
+                    currentSong += 1
+                # Switch to next song in Ardour
+                print('   Switching to song %d in Ardour'%currentSong)
+                ardour_switchSong(currentSong)
+                # Return program event to switch Mididings scene
+                print('   Switching to scene %d'%(currentSong+1))
+                return  event.ProgramEvent(1,1,currentSong+1)
             
-        elif n.data1 == cc_toCurrentSongStart:
-            # Goto current song start
-            print('CC received: to current song start')
-            ardour_switchSong(currentSong)
+            elif n.data1 == cc_prev:
+                # Previoius song
+                print('CC received: Previous song')
+                if currentSong > 0:
+                    currentSong -= 1
+                # Switch to previous song in Ardour
+                print('   Switching to song %d in Ardour'%currentSong)
+                ardour_switchSong(currentSong)
+                # Return program event to switch Mididings scene
+                print('   Switching to scene %d'%(currentSong+1))
+                return event.ProgramEvent(1,1,currentSong+1)
+            
+            elif n.data1 == cc_play:
+                # Play
+                print('CC received: Play')
+                ardour_play()
+            
+            elif n.data1 == cc_playPause:
+                # Play/Pause
+                print('CC received: Play/Pause')
+                ardour_playPause()
+            
+            elif n.data1 == cc_stop:
+                # Stop
+                print('CC received: Stop')
+                ardour_stop()
+                
+            elif n.data1 == cc_toCurrentSongStart:
+                # Goto current song start
+                print('CC received: to current song start')
+                ardour_switchSong(currentSong)
+            # end if n.data1
+        # end if n.data2
+    #end if n.type
             
     elif (n.type == PROGRAM):
         if n.program-1 < len(songs):
@@ -171,12 +181,37 @@ for song in songs:
 
 control = Process(mididingsProcess) >> Filter(PROGRAM) >> SceneSwitch()
 
-# -----------------------------------------------------------------------------
+config(backend=backend,
+       in_ports=[('in',autoconnect)])
 
+# -----------------------------------------------------------------------------
+# Print some info for the user
 print('\nArdour Song Switcher\n')
 print('Use Livedings if you need a visual indicator')
 print('(However, clicking on the Livedings interface will not switch songs)')
-print('Remember to connect the MIDI input port.')
+print('')
+print('Number of tracks: %d'%numtracks)
+temp = ''
+for i in ignore:
+    temp += '%d  '%i
+print('Tracks to ignore: %s'%temp)
+print('Songs:')
+k = 1
+for i in songs:
+    temp = ''
+    for j in i[2]:
+        temp += '%d  '%j
+    print('   %d) %s: Marker %d, Tracks %s'%(k,i[0],i[1],temp))
+    k += 1
+print('')
+print('Use the following CC messages:')
+print('   CC%d: Next song'%cc_next)
+print('   CC%d: Prev song'%cc_prev)
+print('   CC%d: Play'%cc_play)
+print('   CC%d: PlayPause'%cc_playPause)
+print('   CC%d: Stop'%cc_stop)
+print('   CC%d: Go to start of currnent song'%cc_toCurrentSongStart)
+print('You can also switch songs by sending Program messages')
 print('\n')
 
 
